89da5deb140fd21a53d1083b72b2ccf5ddaa5b8f
圣诞结 <2839050301@qq.com>
2025-04-15 21:10:09 +0800

    第一次

---
+++ website/website/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    // ...其他模块
+    FormsModule,
+    HttpClientModule
+  ]
+})
